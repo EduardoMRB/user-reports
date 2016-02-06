9a5c6cda0234cb274c649b3894ee5782c5254193
Eduardo Borges <eduardomrb@gmail.com>
2016-02-06 15:04:18 -0300

    Set initial configuration for routing and pushy
    When a ticket-row is clicked, the pages renders a detailed descritpion
    of the ticket report with a contextual navigation on top.
    
    The actual urls aren't changnig yet, have to examine this issue more
    deeply.

--- src/user_reports/core.cljs
+++ src/user_reports/core.cljs
@@ -1,31 +1,58 @@
 (ns user-reports.core
   (:require-macros [reagent.ratom :refer [reaction]])
   (:require [reagent.core :as reagent :refer [atom]]
-            [re-frame.core :as rf]))
+            [re-frame.core :as rf]
+            [user-reports.route :as route]))
 
 (enable-console-print!)
 
-(defonce app-state (atom {:text "Hello world!"
-                          :tickets [{:title "Problemas no filtro de eventos"
-                                     :description "Quando filtro por data, o sistema apresenta um erro"
-                                     :type "bug"
-                                     :app "Potência"
-                                     :issued-at #inst "2016-02-03"
-                                     :issued-by "Carlos Andrade"
-                                     :images [{:title "screenshot"
-                                               :url "http://whatbettyknows.com/wp-content/uploads/2014/03/FB-DST-bug.jpg"}]
-                                     :open? true
-                                     :responded? false}
-                                    {:title "Fonte do site"
-                                     :description "Trocar fonte do site para Helvetica"
-                                     :type "suggestion"
-                                     :app "Potência"
-                                     :issued-at #inst "2016-02-02"
-                                     :issued-by "Nora Ramos"
-                                     :images [{:title "screenshot"
-                                               :url "http://whatbettyknows.com/wp-content/uploads/2014/03/FB-DST-bug.jpg"}]
-                                     :open? false
-                                     :responded? true}]}))
+(defonce db {:text "Hello world!"
+             :tickets [{:id 1
+                        :title "Problemas no filtro de eventos"
+                        :description "Quando filtro por data, o sistema apresenta um erro"
+                        :type "bug"
+                        :app "Potência"
+                        :issued-at #inst "2016-02-03"
+                        :issued-by "Carlos Andrade"
+                        :images [{:title "screenshot"
+                                  :url "http://whatbettyknows.com/wp-content/uploads/2014/03/FB-DST-bug.jpg"}]
+                        :open? true
+                        :responded? false}
+                       {:id 2
+                        :title "Fonte do site"
+                        :description "Trocar fonte do site para Helvetica"
+                        :type "suggestion"
+                        :app "Potência"
+                        :issued-at #inst "2016-02-02"
+                        :issued-by "Nora Ramos"
+                        :images [{:title "screenshot"
+                                  :url "http://whatbettyknows.com/wp-content/uploads/2014/03/FB-DST-bug.jpg"}]
+                        :open? false
+                        :responded? true}]})
+
+(rf/register-handler
+ :initialize-db
+ (fn [_ _] db))
+
+(rf/register-sub
+ :tickets
+ (fn [db _]
+   (reaction (:tickets @db))))
+
+(rf/register-sub
+ :db
+ (fn [db _]
+   (reaction @db)))
+
+(rf/register-handler
+ :index
+ (fn [db _]
+   (dissoc db :active-ticket)))
+
+(rf/register-handler
+ :ticket-clicked
+ (fn [db [_ ticket-id]]
+   (assoc db :active-ticket {:id ticket-id})))
 
 (defn navbar []
   [:nav.navbar.navbar-inverse.navbar-fixed-top
@@ -41,8 +68,12 @@
 (defn- done? [ticket]
   (not (:open? ticket)))
 
+(defn- localize-date [^Date date]
+  (.toLocaleDateString date))
+
 (defn ticket-row [ticket]
-  [:a.list-group-item {:href "#"}
+  [:a.list-group-item {:href "#"
+                       :on-click #(rf/dispatch [:ticket-clicked (:id ticket)])}
    [:span {:class (case (:type ticket)
                     "bug" "fa fa-bug bug item-badge"
                     "suggestion" "fa fa-microphone item-badge")}]
@@ -52,25 +83,57 @@
                                                 "initial")}}
     (:title ticket)]
    [:span.list-text.text-muted (str " - " (:app ticket))]
-   [:span.list-text.badge (.toLocaleDateString (:issued-at ticket))]
+   [:span.list-text.badge (localize-date (:issued-at ticket))]
    (when (seq (:images ticket))
      [:span.pull-right
       [:span.glyphicon.glyphicon-paperclip]])])
 
-(defn tickets-app []
+(defn ticket-card [ticket]
+  (println ticket)
   [:div
-   [navbar]
-   [:div.spacer {:style {:height "48px"}}]
-   [:div.container.main
-    [toolbar]
-    [:hr]
-    [:div.row
-     [:div.list-group
-      (for [ticket (:tickets @app-state)]
-        [ticket-row ticket])]]]])
+   [:h1 (:title ticket)]
+   [:p (:description ticket)]
+   [:p (str "Reportado por: " (:issued-by ticket)
+            " em: " (localize-date (:issued-at ticket)))]])
 
-(reagent/render-component [tickets-app]
-                          (. js/document (getElementById "app")))
+(defn fetch-ticket [db id]
+  (->> @db
+       :tickets
+       (filter #(= (:id %) id))
+       (first)))
+
+(defn navigation []
+  (let [db (rf/subscribe [:db])]
+    (when (:active-ticket @db)
+      [:div.container
+       [:div.row
+        [:div.btn-group
+         [:button.btn.btn-default {:on-click #(rf/dispatch [:index])}
+          [:span.glyphicon.glyphicon-arrow-left " Voltar"]]]]])))
+
+(defn tickets-app []
+  (let [db (rf/subscribe [:db])]
+    [:div
+     ^{:key "navbar"} [navbar]
+     [:div.spacer {:style {:height "48px"}}]
+     [:div.container.main
+      ^{:key "navigation"} [navigation]
+      (if (:active-ticket @db)
+        (let [active-ticket (-> @db :active-ticket :id)]
+          ^{:key (str "ticket-" active-ticket)} [ticket-card (fetch-ticket db active-ticket)])
+        (list
+         ^{:key "toolbar"} [toolbar]
+         [:hr]
+         [:div.row
+          [:div.list-group
+           (for [ticket (:tickets @db)]
+             ^{:key (str "row-" (:title ticket))} [ticket-row ticket])]]))]]))
+
+(defn ^:export init []
+  (rf/dispatch-sync [:initialize-db])
+  (route/init)
+  (reagent/render-component [tickets-app]
+                            (. js/document (getElementById "app"))))
 
 
 (defn on-js-reload []
